cf91d0a9e7623fcfba42f47bee2bb55c1bbe19b1
Bharani <bharani@zakapps.com>
2025-10-08 17:15:18 +0530

    github automation added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/VP-POC/",
   optimizeDeps: {
     include: ["zustand"], // Ensures the right version is bundled
   },
@@ -13,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
